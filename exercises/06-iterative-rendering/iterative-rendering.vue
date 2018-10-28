e64c8a27da4f314648d42b228485aa468b14221e
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-10-28 12:50:52 +0530

    Iterative rendering

--- exercises/06-iterative-rendering/iterative-rendering.vue
+++ exercises/06-iterative-rendering/iterative-rendering.vue
@@ -3,32 +3,32 @@ export default {
   data() {
     return {
       person: {
-        firstName: 'John',
-        lastName: 'Doe',
+        firstName: "John",
+        lastName: "Doe",
         age: 30
       },
-      fruits: ['Apple', 'Mango'],
+      fruits: ["Apple", "Mango"],
       range: 4
-    }
+    };
   }
-}
+};
 </script>
 
 <template>
   <div class="iterative-rendering">
     <div data-test="fruits">
       <!-- render items from fruits here -->
-      <div data-test="item"></div>
+      <div data-test="item" v-for="fruit in fruits" :key="fruit">{{fruit}}</div>
     </div>
 
     <div data-test="person">
       <!-- render properties from person here -->
-      <div data-test="property"></div>
+      <div data-test="property" v-for="(value, key) in person">{{key}}: {{value}}</div>
     </div>
 
     <div data-test="range">
       <!-- render range here -->
-      <div data-test="item"></div>
+      <div data-test="item" v-for="item in range"></div>
     </div>
   </div>
 </template>
