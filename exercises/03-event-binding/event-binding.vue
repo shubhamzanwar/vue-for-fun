555c75624373bc9bff7351096e8fad4283198d0d
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-10-28 11:24:48 +0530

    Event handling: Adding event handlers for click and mouse down

--- exercises/03-event-binding/event-binding.vue
+++ exercises/03-event-binding/event-binding.vue
@@ -4,12 +4,17 @@ export default {
     handleClick() {},
     handleMouseDown() {}
   }
-}
+};
 </script>
 
 <template>
   <div class="event-binding">
-    <input type="text" data-test="input" />
+    <input
+      type="text"
+      data-test="input"
+      v-on:click="handleClick"
+      v-on:mousedown="(e) => handleMouseDown()"
+    />
   </div>
 </template>
 
