fbf8d999397a27307411da927846ad5493919943
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-10-28 12:18:18 +0530

    conditional rendering: Displaying the greeting pertaining to time of the day

--- exercises/05-conditional-rendering/conditional-rendering.vue
+++ exercises/05-conditional-rendering/conditional-rendering.vue
@@ -3,16 +3,16 @@ export default {
   data() {
     return {
       timeOfDay: 8
-    }
+    };
   }
-}
+};
 </script>
 
 <template>
   <div class="greet">
-    <span>Good Morning</span>
-    <span>Good Afternoon</span>
-    <span>Good Evening</span>
+    <span v-if="timeOfDay < 10">Good Morning</span>
+    <span v-else-if="timeOfDay < 17">Good Afternoon</span>
+    <span v-else>Good Evening</span>
   </div>
 </template>
 
